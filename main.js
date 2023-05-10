5ba2f85b2e12aae2d5a5298c7987be7e82643268
Juli0n21 <julianschlueterbusiness21@gmail.com>
2023-05-10 22:33:34 +0200

    added creating of box using controller semi functional

--- main.js
+++ main.js
@@ -63,6 +63,7 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    renderer.xr.enabled = true;
 
     window.addEventListener('resize', onWindowResize);
 
@@ -73,39 +74,196 @@ function init() {
     camera.position.set(0, 1.6, 0);
     controls.target = new THREE.Vector3(0, 1, -1.8);
 
+    document.body.appendChild(VRButton.createButton(renderer));
+
+
+
+    // controllers
+
+    let positionAfterRelease = new THREE.Vector3();
+    let positionBeforePress = new THREE.Vector3();
+    // Function to handle selectstart event
+    function onSelectStart(event) {
+
+        console.log(event);
+        this.userData.isSelecting = true;
+        const controller = event.data;
+        const buttonIndex = 0; // Index of the button you want to track
+
+        console.log(this);
+
+        if (controller.gamepad.buttons[buttonIndex].pressed) {
+            // The button was pressed on this controller
+            // Save the position before the button press
+            positionBeforePress.copy(this.position);
+        }
+    }
+
+    // Function to handle selectend event
+    function onSelectEnd(event) {
+        this.userData.isSelecting = false;
+        const controller = event.data;
+        const buttonIndex = 0; // Index of the button you want to track
+
+        if (!controller.gamepad.buttons[buttonIndex].pressed) {
+            // The button was released on this controller
+            // Save the position after the button release
+            positionAfterRelease.copy(this.position);
+
+            // Perform any desired action with the recorded positions
+            console.log('Position before button press:', positionBeforePress);
+            console.log('Position after button release:', positionAfterRelease);
+            room.add(convert2postobox(positionBeforePress, positionAfterRelease));
+
+            // Reset the positionAfterRelease to null
+            positionAfterRelease = new THREE.Vector3();
+        }
+    }
+
+    controller1 = renderer.xr.getController(0);
+    controller1.addEventListener('selectstart', onSelectStart);
+    controller1.addEventListener('selectend', onSelectEnd);
+    controller1.addEventListener('connected', function (event) {
+
+        this.add(buildController(event.data));
+
+    });
+    controller1.addEventListener('disconnected', function () {
+
+        this.remove(this.children[0]);
+
+    });
+    scene.add(controller1);
+
+    controller2 = renderer.xr.getController(1);
+    controller2.addEventListener('selectstart', onSelectStart);
+    controller2.addEventListener('selectend', onSelectEnd);
+    controller2.addEventListener('connected', function (event) {
+
+        this.add(buildController(event.data));
+
+    });
+    controller2.addEventListener('disconnected', function () {
+
+        this.remove(this.children[0]);
+
+    });
+    scene.add(controller2);
+
+    const controllerModelFactory = new XRControllerModelFactory();
+
+    controllerGrip1 = renderer.xr.getControllerGrip(0);
+    controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
+    scene.add(controllerGrip1);
+
+    controllerGrip2 = renderer.xr.getControllerGrip(1);
+    controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
+    scene.add(controllerGrip2);
+
+    scene.add(convert2postobox(new THREE.Vector3(1, 1, 1), new THREE.Vector3(2, 2, 2)));
+    var point1 = new THREE.Vector3(1, 1, 1);
+    var point2 = new THREE.Vector3(2, 2, 2);
+
+    var midpoint = new THREE.Vector3().addVectors(point1, point2).multiplyScalar(0.5);
+
+    var distance = point1.distanceTo(point2);
+
+    var geometry = new THREE.BoxGeometry(distance, distance, distance);
+
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+    var cube = new THREE.Mesh(geometry, material);
+    cube.position.set(midpoint.x, midpoint.y, midpoint.z);
+    room.add(cube);
+
 
 }
-
 function handleController(controller) {
 
     checkforcollisions(controller);
+
     if (controller.userData.isSelecting) {
-        convert2postobox(prevpos,controller.position);
+
+
     }
-    else {
-        prevpos = controller.position;
+
+
+}
+
+function buildController(data) {
+
+    let geometry, material;
+
+    switch (data.targetRayMode) {
+
+        case 'tracked-pointer':
+
+            geometry = new THREE.BufferGeometry();
+            geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0, 0, 0, - 1], 3));
+            geometry.setAttribute('color', new THREE.Float32BufferAttribute([0.5, 0.5, 0.5, 0, 0, 0], 3));
+
+            material = new THREE.LineBasicMaterial({ vertexColors: true, blending: THREE.AdditiveBlending });
+
+            return new THREE.Line(geometry, material);
+
+        case 'gaze':
+
+            geometry = new THREE.RingGeometry(0.02, 0.04, 32).translate(0, 0, - 1);
+            material = new THREE.MeshBasicMaterial({ opacity: 0.5, transparent: true });
+            return new THREE.Mesh(geometry, material);
+
     }
 
 }
 
-function convert2postobox(point1, point2){
+
+function convert2postobox(point1, point2) {
+
     var midpoint = new THREE.Vector3().addVectors(point1, point2).multiplyScalar(0.5);
+
     var distance = point1.distanceTo(point2);
 
-// Create a cube geometry
-var geometry = new THREE.BoxGeometry(distance, distance, distance);
+    var geometry = new THREE.BoxGeometry(distance, distance, distance);
 
-// Create a material
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-// Create a mesh using the geometry and material
-var cube = new THREE.Mesh(geometry, material);
+    var cube = new THREE.Mesh(geometry, material);
 
-cube.position.set(midpoint);
+    cube.position.set(midpoint.x, midpoint.y, midpoint.z);
+    console.log(cube);
+    return cube;
 
-scene.add(cube);
+}
 
-return cube;
+function checkforcollisions() {
+
+    tempMatrix.identity().extractRotation(controller1.matrixWorld);
+
+    raycaster.ray.origin.setFromMatrixPosition(controller1.matrixWorld);
+    raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
+
+    const intersects = raycaster.intersectObjects(room.children, false);
+
+    if (intersects.length > 0) {
+
+        if (INTERSECTED != intersects[0].object) {
+
+
+            if (INTERSECTED) INTERSECTED.material.color.set(INTERSECTED.currentHex);
+
+            INTERSECTED = intersects[0].object;
+            INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+            INTERSECTED.material.color.set(0xff0000);
+
+        }
+
+    } else {
+
+        if (INTERSECTED) INTERSECTED.material.color.set(INTERSECTED.currentHex);
+
+        INTERSECTED = undefined;
+
+    }
 
 }
 
@@ -129,8 +287,8 @@ function render() {
     const delta = clock.getDelta() * 60;
     ThreeMeshUI.update();
 
-  //  handleController(controller1);
-  //  handleController(controller2);
+    handleController(controller1);
+    handleController(controller2);
     renderer.render(scene, camera);
 
     stats.update();
