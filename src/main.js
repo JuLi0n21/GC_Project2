5bf88d037cc644f9e3f80299e4a1f0f28cdc7831
Juli0n21 <julianschlueterbusiness21@gmail.com>
2023-05-17 09:46:18 +0200

    oen controller works

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import { InteractiveGroup } from 'three/addons/interactive/InteractiveGroup.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import ThreeMeshUI from 'three-mesh-ui'
+import { drawgui, updateButtons } from './threegui';
+import VRControl from './utils/VRControl.js';
 
 let camera, scene, renderer, loader, stats, statsMesh, raycaster, controls;
 
@@ -23,7 +25,7 @@ let gamepad;
 let gamepadAxes;
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
-let group;
+let group, vrControl;
 
 const clock = new THREE.Clock();
 
@@ -32,204 +34,133 @@ animate();
 
 function init() {
 
-    raycaster = new THREE.Raycaster();
+  raycaster = new THREE.Raycaster();
 
-    stats = new Stats();
-    document.body.appendChild(stats.dom);
+  stats = new Stats();
+  document.body.appendChild(stats.dom);
 
-    loader = new FontLoader();
+  loader = new FontLoader();
 
-    scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    scene.background = new THREE.Color(0x505050);
+  scene.background = new THREE.Color(0x505050);
 
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 1.6, 3);
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 1.6, 3);
 
-    room = new THREE.LineSegments(
-        new BoxLineGeometry(6, 6, 6, 10, 10, 10),
-        new THREE.LineBasicMaterial({ color: 0x808080 })
-    );
-    room.geometry.translate(0, 3, 0);
-    scene.add(room);
+  room = new THREE.LineSegments(
+    new BoxLineGeometry(6, 6, 6, 10, 10, 10),
+    new THREE.LineBasicMaterial({ color: 0x808080 })
+  );
+  room.geometry.translate(0, 3, 0);
+  scene.add(room);
 
-    scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
+  scene.add(new THREE.HemisphereLight(0x606060, 0x404040));
 
-    const light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
-    //
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(1, 1, 1).normalize();
+  scene.add(light);
+  //
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.xr.enabled = true;
-    renderer.xr.setReferenceSpaceType( 'local' );
-    document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.xr.enabled = true;
+  renderer.xr.setReferenceSpaceType('local');
+  document.body.appendChild(renderer.domElement);
 
-    //
+  //
 
-    document.body.appendChild(VRButton.createButton(renderer));
+  document.body.appendChild(VRButton.createButton(renderer));
 
-    // Orbit controls for no-vr
+  // Orbit controls for no-vr
 
-    controls = new OrbitControls(camera, renderer.domElement);
-    camera.position.set(0, 1.6, 0);
-    controls.target = new THREE.Vector3(0, 1, -1.8);
+  controls = new OrbitControls(camera, renderer.domElement);
+  camera.position.set(0, 1.6, 0);
+  controls.target = new THREE.Vector3(0, 1, -1.8);
 
-    // controllers
-    let positionBeforePress = new THREE.Vector3;
-    let positionAfterRelease = new THREE.Vector3;
+  // controllers
 
-    function onSelectStart(event) {
+  let positionBeforePress = new THREE.Vector3;
+  let positionAfterRelease = new THREE.Vector3;
 
-        this.userData.isSelecting = true;
-        const controller = event.data;
-        const buttonIndex = 0;
 
-        if(controller.gamepad.buttons[buttonIndex].pressed){
-          positionBeforePress.copy(this.position);
-        }
-    }
+  vrControl = VRControl(renderer, camera, scene);
+  for (let i = 0; i < 2; i++) {
 
-    function onSelectEnd(event) {
+
+    scene.add(vrControl.controllerGrips[i], vrControl.controllers[i]);
+
+    vrControl.controllers[i].addEventListener('selectstart', (event) => {
+
 
       const controller = event.data;
       const buttonIndex = 0;
 
-      if(!controller.gamepad.buttons[buttonIndex].pressed){
-        positionAfterRelease.copy(this.position);
-        
-        Convert2postobox(positionBeforePress,positionAfterRelease);
+      if (controller.gamepad.buttons[buttonIndex].pressed) {
+        positionBeforePress.copy(vrControl.controllers[i].position);
       }
-
-    }
-
-    controller1 = renderer.xr.getController(0);
-    controller1.addEventListener('selectstart', onSelectStart);
-    controller1.addEventListener('selectend', onSelectEnd);
-    controller1.addEventListener('connected', function (event) {
-
-        this.add(buildController(event.data));
+      vrControl.controllers[i].userData.selected = true;
 
     });
-    controller1.addEventListener('disconnected', function () {
+    vrControl.controllers[i].addEventListener('selectend', (event) => {
 
-        this.remove(this.children[0]);
+      const controller = event.data;
+      const buttonIndex = 0;
 
+      if (!controller.gamepad.buttons[buttonIndex].pressed) {
+        positionAfterRelease.copy(vrControl.controllers[i].position);
+
+        Convert2postobox(positionBeforePress, positionAfterRelease);
+      }
+      vrControl.controllers[i].userData.selected = false;
     });
-    scene.add(controller1);
-    
-    controller2 = renderer.xr.getController(1);
-    controller2.addEventListener('selectstart', onSelectStart);
-    controller2.addEventListener('selectend', onSelectEnd);
-    controller2.addEventListener('connected', function (event) {
+  }
 
-        this.add(buildController(event.data));
 
-    });
-    controller2.addEventListener('disconnected', function () {
+  window.addEventListener('resize', onWindowResize);
 
-        this.remove(this.children[0]);
+  group = new THREE.Group();
+  scene.add(group);
 
-    });
-    scene.add(controller2);
-
-    const controllerModelFactory = new XRControllerModelFactory();
-
-    controllerGrip1 = renderer.xr.getControllerGrip(0);
-    controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
-    scene.add(controllerGrip1);
-
-    controllerGrip2 = renderer.xr.getControllerGrip(1);
-    controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
-    scene.add(controllerGrip2);
-
-    
-    const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
-
-    const line = new THREE.Line( geometry );
-    line.name = 'line';
-    line.scale.z = 5;
-
-    controller1.add( line.clone() );
-    controller2.add( line.clone() );
-
-    window.addEventListener('resize', onWindowResize);
-
-    group = new THREE.Group();
-    scene.add( group );
+  drawgui(scene);
 
 }
 
-function buildController(data) {
-
-    let geometry, material;
-
-    switch (data.targetRayMode) {
-
-        case 'tracked-pointer':
-
-            geometry = new THREE.BufferGeometry();
-            geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0, 0, 0, - 1], 3));
-            geometry.setAttribute('color', new THREE.Float32BufferAttribute([0.5, 0.5, 0.5, 0, 0, 0], 3));
-
-            material = new THREE.LineBasicMaterial({ vertexColors: true, blending: THREE.AdditiveBlending });
-
-            return new THREE.Line(geometry, material);
-
-        case 'gaze':
-
-            geometry = new THREE.RingGeometry(0.02, 0.04, 32).translate(0, 0, - 1);
-            material = new THREE.MeshBasicMaterial({ opacity: 0.5, transparent: true });
-            return new THREE.Mesh(geometry, material);
-
-    }
-
-}
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-}
-
-function handleController(controller) {
-
-   //  checkforcollisions(controller);
-    if (controller.userData.isSelecting) {
-
-    }
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
-function intersectObjects( controller ) {
 
+
+function intersectObjects(controller) {
+//console.log(controller.userData)
   // Do not highlight in mobile-ar
 
-  if ( controller.userData.targetRayMode === 'screen' ) return;
+  if (controller.userData.targetRayMode === 'screen') return;
 
   // Do not highlight when already selected
 
-  if ( controller.userData.selected !== undefined ) return;
+  if (controller.userData.selected !== undefined) return;
 
-  
+  const line = controller.getObjectByName('line');
 
-  const line = controller.getObjectByName( 'line' );
+  const intersections = getIntersections(controller);
 
-  const intersections = getIntersections( controller );
-  console.log(intersections)
+  //console.log(intersections)
+  if (intersections.length > 0) {
 
-  if ( intersections.length > 0 ) {
-
-    const intersection = intersections[ 0 ];
+    const intersection = intersections[0];
 
     const object = intersection.object;
     object.material.emissive.r = 1;
-    intersected.push( object );
+    intersected.push(object);
 
   } else {
 
@@ -237,22 +168,22 @@ function intersectObjects( controller ) {
 
 }
 
-function getIntersections( controller ) {
+function getIntersections(controller) {
 
   controller.updateMatrixWorld();
 
-  tempMatrix.identity().extractRotation( controller.matrixWorld );
+  tempMatrix.identity().extractRotation(controller.matrixWorld);
 
-  raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
-  raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-
-  return raycaster.intersectObjects( group.children, false );
+  raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+  raycaster.ray.direction.set(0, 0, - 1).applyMatrix4(tempMatrix);
+  //console.log(group.children)
+  return raycaster.intersectObjects(group.children, false);
 
 }
 
 function cleanIntersected() {
 
-  while ( intersected.length ) {
+  while (intersected.length) {
 
     const object = intersected.pop();
     object.material.emissive.r = 0;
@@ -267,30 +198,30 @@ function Convert2postobox(point1, point2) {
   var distance = point1.distanceTo(point2);
 
   const geometries = [
-    new THREE.BoxGeometry( distance, distance, distance ),
-    new THREE.ConeGeometry( 0.2, 0.2, 64 ),
-    new THREE.CylinderGeometry( 0.2, 0.2, 0.2, 64 ),
-    new THREE.IcosahedronGeometry( 0.2, 8 ),
-    new THREE.TorusGeometry( 0.2, 0.04, 64, 32 )
+    new THREE.BoxGeometry(distance, distance, distance),
+    new THREE.ConeGeometry(0.2, 0.2, 64),
+    new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
+    new THREE.IcosahedronGeometry(0.2, 8),
+    new THREE.TorusGeometry(0.2, 0.04, 64, 32)
   ];
 
- // const geometry = geometries[ Math.floor( Math.random() * geometries.length ) ];
- const geometry = geometries[0]
+  // const geometry = geometries[ Math.floor( Math.random() * geometries.length ) ];
+  const geometry = geometries[0]
 
-  const material = new THREE.MeshStandardMaterial( {
+  const material = new THREE.MeshStandardMaterial({
     color: Math.random() * 0xffffff,
     roughness: 0.7,
     metalness: 0.0
-  } );
+  });
 
-  const object = new THREE.Mesh( geometry, material );
+  const object = new THREE.Mesh(geometry, material);
 
-  object.scale.setScalar( Math.random() + 0.5 );
+  object.scale.setScalar(Math.random() + 0.5);
 
   object.castShadow = true;
   object.receiveShadow = true;
 
-  group.add( object );
+  group.add(object);
 
 
   object.position.copy(midpoint);
@@ -300,23 +231,27 @@ function Convert2postobox(point1, point2) {
 
 function animate() {
 
-    renderer.setAnimationLoop(render);
+  renderer.setAnimationLoop(render);
 
 }
 
 function render() {
 
-    const delta = clock.getDelta() * 60;
-    ThreeMeshUI.update();
+  const delta = clock.getDelta() * 60;
+  ThreeMeshUI.update();
 
-    
-    cleanIntersected();
 
-    intersectObjects( controller1 );
-    intersectObjects( controller2 );
+  cleanIntersected();
 
-    renderer.render(scene, camera);
+  
+  updateButtons(renderer, vrControl,0);
 
-    stats.update();
+  intersectObjects(vrControl.controllers[0]);
+  intersectObjects(vrControl.controllers[1]);
+
+
+  renderer.render(scene, camera);
+
+  stats.update();
 
 }
--- src/threegui.js
+++ src/threegui.js
@@ -1,109 +1,298 @@
+import ThreeMeshUI from 'three-mesh-ui'
+import * as THREE from 'three';
 
-const gui = new GUI({ width: 300 });
-//camera controls
-const gui_camera = gui.addFolder("CameraControls");
-gui_camera.add(camera.rotation, 'x', 0, Math.PI * 2);
-gui_camera.add(camera.rotation, 'y', 0, Math.PI * 2);
-gui_camera.add(camera.rotation, 'z', 0, Math.PI * 2);
-const gui_camera_pos = gui.addFolder("CameraPos");
-gui_camera_pos.add(camera.position, "x", -100, 100);
-gui_camera_pos.add(camera.position, "y", -100, 100);
-gui_camera_pos.add(camera.position, "z", -0, 1000);
-gui_camera.open();
-gui_camera_pos.open();
-const gui_interactions = gui.addFolder("Actions")
-
-gui_interactions.add(params, "newdots")
-gui_interactions.add(params, "point_color", Object.keys(pointcolors));
-
-gui_interactions.add(params, "connectdots");
-gui_interactions.add(params, "line_color", Object.keys(linecolors));
+const raycaster = new THREE.Raycaster
+const objsToTest = []
+let selectState = false;
 
 
-gui_interactions.add(params, "pointcount", 0, 100)
-gui_interactions.open();
+export function drawgui(scene) {
+    const container = new ThreeMeshUI.Block({
+        height: 1.5,
+        width: 1
+    });
 
-//boxes 
+    container.position.set(0, 1, -1.8);
+    container.rotation.x = -0.55;
+    scene.add(container);
 
-const gui_interactions_boxes = gui_interactions.addFolder("Boxes");
+    const imageBlock = new ThreeMeshUI.Block({
+        height: 1,
+        width: 1,
+        offset: 0.01 // distance separating the inner block from its parent
+    });
 
-gui_interactions_boxes.add(boxfunctions, "addbox");
-gui_interactions_boxes.add(boxfunctions, "removebox");
-gui_interactions_boxes.add(boxfunctions, "box_color", Object.keys(boxcolors));
+    const textBlock = new ThreeMeshUI.Block({
+        height: 0.4,
+        width: 0.8,
+        margin: 0.05, // like in CSS, horizontal and vertical distance from neighbour
+        offset: 0.01 // distance separating the inner block from its parent
+    });
+
+    container.add(imageBlock, textBlock);
+
+    container.set({
+        fontFamily: '../assests/Roboto-msdf.json',
+        fontTexture: '../assests/Roboto-msdf.png',
+    });
+
+    const text = new ThreeMeshUI.Text({
+        content: 'The spiny bush viper is known for its extremely keeled dorsal scales.'
+    });
+
+    textBlock.add(text);
+
+    text.set({
+        fontColor: new THREE.Color(0xd2ffbd),
+        fontSize: 0.04
+    });
+
+    textBlock.set({
+        // alignContent: 'right', // could be 'center' or 'left'
+        // alignContent has been deprecated, rely on alignItems or textAlign
+        textAlign: 'right',
+        justifyContent: 'end', // could be 'start', 'center', 'end', 'space-between', 'space-around', 'space-evenly'  
+        padding: 0.03
+    });
+
+    
+	const meshContainer = new THREE.Group();
+	meshContainer.position.set( 0, 1, -1.9 );
+	scene.add( meshContainer );
+
+	//
+
+	const sphere = new THREE.Mesh(
+		new THREE.IcosahedronBufferGeometry( 0.3, 1 ),
+		new THREE.MeshStandardMaterial( { color: 0x3de364, flatShading: true } )
+	);
+
+	const box = new THREE.Mesh(
+		new THREE.BoxBufferGeometry( 0.45, 0.45, 0.45 ),
+		new THREE.MeshStandardMaterial( { color: 0x643de3, flatShading: true } )
+	);
+
+	const cone = new THREE.Mesh(
+		new THREE.ConeBufferGeometry( 0.28, 0.5, 10 ),
+		new THREE.MeshStandardMaterial( { color: 0xe33d4e, flatShading: true } )
+	);
+
+	//
 
 
-gui.add(params, "resetcam");
-gui.add(params, "clear");
-gui.domElement.style.visibility = 'hidden';
+	sphere.visible = box.visible = cone.visible = false;
 
-const group = new InteractiveGroup(renderer, camera);
-scene.add(group);
+	meshContainer.add( sphere, box, cone );
 
-const mesh = new HTMLMesh(gui.domElement);
-mesh.position.x = - 0.75;
-mesh.position.y = 1.5;
-mesh.position.z = - 0.5;
-mesh.rotation.y = Math.PI / 4;
-mesh.scale.setScalar(2);
-group.add(mesh);
+	const meshes = [ sphere, box, cone ];
+	let currentMesh = 0;
 
-stats = new Stats();
-stats.dom.style.width = '80px';
-stats.dom.style.height = '48px';
-document.body.appendChild(stats.dom);
+	showMesh( currentMesh );
 
-statsMesh = new HTMLMesh(stats.dom);
-statsMesh.position.x = - 0.75;
-statsMesh.position.y = 2.1;
-statsMesh.position.z = - 0.6;
-statsMesh.rotation.y = Math.PI / 4;
-statsMesh.scale.setScalar(2.5);
-group.add(statsMesh);
+	//////////
+	// Panel
+	//////////
 
-const container = new ThreeMeshUI.Block({
-    height: 1.5,
-    width: 1
-});
+	makePanel();
 
-container.position.set(0, 1, -1.8);
-container.rotation.x = -0.55;
-scene.add(container);
+    function showMesh( id ) {
 
-const imageBlock = new ThreeMeshUI.Block({
-    height: 1,
-    width: 1,
-    offset: 0.01 // distance separating the inner block from its parent
-});
+        meshes.forEach( ( mesh, i ) => {
+    
+            mesh.visible = i === id ? true : false;
+    
+        } );
+    
+    }
 
-const textBlock = new ThreeMeshUI.Block({
-    height: 0.4,
-    width: 0.8,
-    margin: 0.05, // like in CSS, horizontal and vertical distance from neighbour
-    offset: 0.01 // distance separating the inner block from its parent
-});
+    function makePanel() {
 
-container.add(imageBlock, textBlock);
+        // Container block, in which we put the two buttons.
+        // We don't define width and height, it will be set automatically from the children's dimensions
+        // Note that we set contentDirection: "row-reverse", in order to orient the buttons horizontally
+    
+        const container = new ThreeMeshUI.Block( {
+            justifyContent: 'center',
+            contentDirection: 'row-reverse',
+            fontFamily: "../assests/Roboto-msdf.json",
+            fontTexture: "../assests/Roboto-msdf.png",
+            fontSize: 0.07,
+            padding: 0.02,
+            borderRadius: 0.11
+        } );
+    
+        container.position.set( 0, 0.6, -1.2 );
+        container.rotation.x = -0.55;
+        scene.add( container );
+    
+        // BUTTONS
+    
+        // We start by creating objects containing options that we will use with the two buttons,
+        // in order to write less code.
+    
+        const buttonOptions = {
+            width: 0.4,
+            height: 0.15,
+            justifyContent: 'center',
+            offset: 0.05,
+            margin: 0.02,
+            borderRadius: 0.075
+        };
+    
+        // Options for component.setupState().
+        // It must contain a 'state' parameter, which you will refer to with component.setState( 'name-of-the-state' ).
+    
+        const hoveredStateAttributes = {
+            state: 'hovered',
+            attributes: {
+                offset: 0.035,
+                backgroundColor: new THREE.Color( 0x999999 ),
+                backgroundOpacity: 1,
+                fontColor: new THREE.Color( 0xffffff )
+            },
+        };
+    
+        const idleStateAttributes = {
+            state: 'idle',
+            attributes: {
+                offset: 0.035,
+                backgroundColor: new THREE.Color( 0x666666 ),
+                backgroundOpacity: 0.3,
+                fontColor: new THREE.Color( 0xffffff )
+            },
+        };
+    
+        // Buttons creation, with the options objects passed in parameters.
+    
+        const buttonNext = new ThreeMeshUI.Block( buttonOptions );
+        const buttonPrevious = new ThreeMeshUI.Block( buttonOptions );
+    
+        // Add text to buttons
+    
+        buttonNext.add(
+            new ThreeMeshUI.Text( { content: 'next' } )
+        );
+    
+        buttonPrevious.add(
+            new ThreeMeshUI.Text( { content: 'previous' } )
+        );
+    
+        // Create states for the buttons.
+        // In the loop, we will call component.setState( 'state-name' ) when mouse hover or click
+    
+        const selectedAttributes = {
+            offset: 0.02,
+            backgroundColor: new THREE.Color( 0x777777 ),
+            fontColor: new THREE.Color( 0x222222 )
+        };
+    
+        buttonNext.setupState( {
+            state: 'selected',
+            attributes: selectedAttributes,
+            onSet: () => {
+    
+                currentMesh = ( currentMesh + 1 ) % 3;
+                showMesh( currentMesh );
+                console.log("Click")
+            }
+        } );
+        buttonNext.setupState( hoveredStateAttributes );
+        buttonNext.setupState( idleStateAttributes );
+    
+        //
+    
+        buttonPrevious.setupState( {
+            state: 'selected',
+            attributes: selectedAttributes,
+            onSet: () => {
+    
+                currentMesh -= 1;
+                if ( currentMesh < 0 ) currentMesh = 2;
+                showMesh( currentMesh );
+    
+            }
+        } );
+        buttonPrevious.setupState( hoveredStateAttributes );
+        buttonPrevious.setupState( idleStateAttributes );
+    
+        //
+    
+        container.add( buttonNext, buttonPrevious );
+        objsToTest.push( buttonNext, buttonPrevious );
+    
+    }
 
-container.set({
-    fontFamily: '../assests/Roboto-msdf.json',
-    fontTexture: '../assests/Roboto-msdf.png',
-});
 
-const text = new ThreeMeshUI.Text({
-    content: 'The spiny bush viper is known for its extremely keeled dorsal scales.'
-});
+}
 
-textBlock.add(text);
+export function updateButtons(renderer,vrControl,controllerID) {
 
-text.set({
-    fontColor: new THREE.Color(0xd2ffbd),
-    fontSize: 0.04
-});
+	// Find closest intersecting object
 
-textBlock.set({
-    // alignContent: 'right', // could be 'center' or 'left'
-    // alignContent has been deprecated, rely on alignItems or textAlign
-    textAlign: 'right',
-    justifyContent: 'end', // could be 'start', 'center', 'end', 'space-between', 'space-around', 'space-evenly'  
-    padding: 0.03
-});
+	let intersect;
+
+	if ( renderer.xr.isPresenting ) {
+
+		vrControl.setFromController( controllerID, raycaster.ray );
+
+		intersect = raycast();
+
+		// Position the little white dot at the end of the controller pointing ray
+		if ( intersect ) vrControl.setPointerAt( controllerID, intersect.point );
+
+	} 
+
+	// Update targeted button state (if any)
+
+	if ( intersect && intersect.object.isUI ) {
+
+		if ( selectState ) {
+
+			// Component.setState internally call component.set with the options you defined in component.setupState
+			intersect.object.setState( 'selected' );
+
+		} else {
+
+			// Component.setState internally call component.set with the options you defined in component.setupState
+			intersect.object.setState( 'hovered' );
+
+		}
+
+	}
+
+	// Update non-targeted buttons state
+
+	objsToTest.forEach( ( obj ) => {
+
+		if ( ( !intersect || obj !== intersect.object ) && obj.isUI ) {
+
+			// Component.setState internally call component.set with the options you defined in component.setupState
+			obj.setState( 'idle' );
+
+		}
+
+	} );
+
+}
+
+function raycast() {
+
+	return objsToTest.reduce( ( closestIntersection, obj ) => {
+
+		const intersection = raycaster.intersectObject( obj, true );
+
+		if ( !intersection[ 0 ] ) return closestIntersection;
+
+		if ( !closestIntersection || intersection[ 0 ].distance < closestIntersection.distance ) {
+
+			intersection[ 0 ].object = obj;
+
+			return intersection[ 0 ];
+
+		}
+
+		return closestIntersection;
+
+	}, null );
+
+}
